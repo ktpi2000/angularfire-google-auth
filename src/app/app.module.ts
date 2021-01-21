6d17b57636d1f22249573364853336bc2f1f5fb2
ktpi2000 <katapi2000@gmail.com>
2021-01-22 01:12:16 +0900

    create secret component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
+import { SecretComponent } from './secret/secret.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecretComponent
   ],
   imports: [
     BrowserModule,
