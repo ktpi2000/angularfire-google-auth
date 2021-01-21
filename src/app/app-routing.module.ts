02eaeea382cd01c9931e28aefba93c68c986c514
ktpi2000 <katapi2000@gmail.com>
2021-01-22 01:25:15 +0900

    ログイン痔にしか表示しない

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SecretComponent } from './secret/secret.component';
+import { AuthGuard } from './services/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'secret', component: SecretComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
