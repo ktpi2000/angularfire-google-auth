50b9d014b3b04bb84f0a9a3921729dea3c734d89
ktpi2000 <katapi2000@gmail.com>
2021-01-22 01:00:04 +0900

    signin & signout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
-import * as firebase from 'firebase/app';
+import firebase from 'firebase/app';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 
@@ -30,6 +30,30 @@ export class AuthService {
           return of(null);  // 空のObservableを返す
         }
       })
-    )
+    );
+  }
+
+  async googleSignin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const credential = await this.angularFireAuth.signInWithPopup(provider);
+    return this.updateUserData(credential.user);
+  }
+
+  async signOut() {
+    await this.angularFireAuth.signOut();
+    this.router.navigate(['/']);
+  }
+
+  private updateUserData(user: User) {
+    // ログイン痔にfirestoreにuser dataを追加する
+    const userRef: AngularFirestoreDocument<User> = this.angularFireStore.doc(`users/${user.uid}`);
+
+    const data = {
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      photoURL: user.photoURL
+    }
+    return userRef.set(data, { merge: true })
   }
 }
